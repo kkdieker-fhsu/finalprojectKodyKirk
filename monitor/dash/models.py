a788b3b5617d9fd19dfb91b8fb8fa1d5e3347102
kkdieker-fhsu <kkdieker@mail.fhsu.edu>
2025-11-07 18:23:08 -0900

    database beginnings

--- monitor/dash/models.py
+++ monitor/dash/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Endpoints(models.Model):
+    ip_address = models.GenericIPAddressField(primary_key=True)
+    mac_address = models.CharField(max_length=17)
+    hostname = models.CharField(max_length=255)
+    last_seen = models.DateTimeField(auto_now=True, auto_now_add=False)
+
+class TrafficLog(models.Model):
+    indata = models.BigIntegerField()
+    outdata = models.BigIntegerField()
+    interval_start = models.DateTimeField(auto_now=True, auto_now_add=False)
+    endpoint = models.ForeignKey(Endpoints, on_delete=models.CASCADE)
