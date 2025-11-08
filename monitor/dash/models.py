87d3559af7ce5e6f42cbc22e78741d22c53656c3
kkdieker-fhsu <kkdieker@mail.fhsu.edu>
2025-11-08 05:40:27 -0900

    database adjustments

--- monitor/dash/models.py
+++ monitor/dash/models.py
@@ -7,8 +7,16 @@ class Endpoints(models.Model):
     hostname = models.CharField(max_length=255)
     last_seen = models.DateTimeField(auto_now=True, auto_now_add=False)
 
+    def __str__(self):
+        return self.ip_address
+
 class TrafficLog(models.Model):
     data_in = models.BigIntegerField()
     data_out = models.BigIntegerField()
     interval_start = models.DateTimeField(auto_now=True, auto_now_add=False)
-    endpoint = models.ForeignKey(Endpoints, on_delete=models.CASCADE)
+    ip_src = models.OneToOneField(Endpoints, on_delete=models.CASCADE, unique=True)
+    ip_dst = models.GenericIPAddressField(unique=True)
+
+    def __str__(self):
+        return self.ip_src
+
