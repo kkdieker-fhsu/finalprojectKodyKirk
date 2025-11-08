8ad035c0e38b77af16c739194edd36be28234da1
kkdieker-fhsu <kkdieker@mail.fhsu.edu>
2025-11-08 12:31:46 -0900

    new form for other table

--- monitor/dash/forms.py
+++ monitor/dash/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from .models import Endpoints
+from .models import Endpoints, TrafficLog
+
 
 class registerendpoint(forms.ModelForm):
     class Meta:
         model = Endpoints
         fields = ['ip_address', 'mac_address', 'hostname']
 
+class registertraffic(forms.ModelForm):
+    class Meta:
+        model = TrafficLog
+        fields = ['ip_src', 'ip_dst', 'data_in', 'data_out']
