fba46913f051a6171ebc42154251bf0b0acb4322
kkdieker-fhsu <kkdieker@mail.fhsu.edu>
2025-12-06 10:56:19 -0900

    better test for user logins

--- monitor/dash/tests.py
+++ monitor/dash/tests.py
@@ -1,7 +1,9 @@
 from django.test import TestCase, Client
 from django.urls import reverse
 from django.utils import timezone
+from django.contrib.auth import get_user_model
 from .models import Endpoints
+
 
 class EndpointModelTests(TestCase):
     def test_endpoint_creation(self):
@@ -12,13 +14,15 @@ class EndpointModelTests(TestCase):
         )
         self.assertEqual(str(e), "192.168.1.50")
 
+
 class ViewTests(TestCase):
     def setUp(self):
         self.client = Client()
-        # mock login for @login_required views
-        self.user = self.client.force_login(
-            user=type('User', (object,), {'is_authenticated': True, 'pk': 1})()
-        )
+
+        User = get_user_model()
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+
+        self.client.force_login(self.user)
 
     def test_dashboard_load(self):
         response = self.client.get(reverse('dash:index'))
