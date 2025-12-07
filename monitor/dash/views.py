c1051e43c16f3ec7c63b6cb2145a2b3404e91283
kkdieker-fhsu <kkdieker@mail.fhsu.edu>
2025-12-07 12:51:53 -0900

    conditional traffic filtering in detail view

--- monitor/dash/views.py
+++ monitor/dash/views.py
@@ -250,9 +250,11 @@ def traffic_upload(request):
 def detail(request, ip_address):
     #finds the endpoint in question and any traffic associated with it
     endpoint = get_object_or_404(Endpoints, pk=ip_address)
-    traffic_src = TrafficLog.objects.filter(ip_src=endpoint)
-    traffic_dst = TrafficLog.objects.filter(ip_dst=endpoint.ip_address)
-    traffic = (traffic_src | traffic_dst).distinct()
+    if endpoint.mac_address == 'Remote':
+        traffic = TrafficLog.objects.filter(ip_dst=endpoint.ip_address)
+
+    else:
+        traffic = TrafficLog.objects.filter(ip_src=endpoint)
     return render(request, "dash/detail.html", {'endpoint': endpoint, 'traffic': traffic})
 
 @login_required
